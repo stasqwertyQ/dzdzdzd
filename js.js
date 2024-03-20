d09cbc480c5b8074474750c2eccac38fe6480892
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2024-03-20 16:10:51 +0200

    da

---
+++ js.js
@@ -0,0 +1,32 @@
+
+const inputAmount = document.getElementById('inputAmount');
+const createButton = document.getElementById('createButton');
+const destroyButton = document.getElementById('destroyButton'); 
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+    const amount = inputAmount.value;
+    clearBoxes();
+    for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+      const size = 30 + i * 10;
+    box.style.backgroundColor = getRandomColor();
+    box.style.width = size + 'px';
+box.style.height = size + 'px';
+box.classList.add('box');
+    boxesContainer.appendChild(box);
+    }
+}
+function clearBoxes() {
+    boxesContainer.innerHTML = '';
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
